48e404ee56ba61a89d6f860934dae121d315f4f4
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-04-01 19:57:14 +0800

    整合freemarker

---
+++ springboot-freemarker/src/main/java/com/lizp/springboot/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.lizp.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping(value = "/index")
+	public String index(ModelMap map) {
+		map.put("title", "点击这里，进入迁移界面");
+		return "index";
+	}
+}
---
+++ springboot-freemarker/src/main/resources/templates/index.ftl
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+<title>首页</title>
+<link href="/css/index.css" rel="stylesheet" />
+</head>
+<body>
+	<center>
+		<img src="/images/logo.png" />
+		<h1 id="title">${title}</h1>
+	</center>
+
+	<script type="text/javascript"
+		src="/webjars/jquery/2.1.4/jquery.min.js"></script>
+</body>
+</html>
