a627858c427092bbee6247e3877b7163f55b035b
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-04-16 14:21:06 +0800

    配置错误修改

--- springboot-actuator/src/main/java/com/lizp/springboot/controller/IndexController.java
+++ springboot-actuator/src/main/java/com/lizp/springboot/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.lizp.springboot.controller;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String index() {
-		return "ok";
+	public String index(ModelMap map) {
+		map.put("title", "欢迎");
+		return "index";
 	}
 
 }
