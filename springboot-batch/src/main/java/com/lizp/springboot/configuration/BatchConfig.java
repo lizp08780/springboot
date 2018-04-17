5e7fbd81430c328e2128fac3bc86a9587bdb90eb
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-04-17 09:21:15 +0800

    优化

--- springboot-batch/src/main/java/com/lizp/springboot/configuration/BatchConfig.java
+++ springboot-batch/src/main/java/com/lizp/springboot/configuration/BatchConfig.java
@@ -1,4 +1,4 @@
-package com.lizp.springboot.bean;
+package com.lizp.springboot.configuration;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecutionListener;
