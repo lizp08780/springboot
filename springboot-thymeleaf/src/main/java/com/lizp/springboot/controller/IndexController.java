61f03dbadcee2a097287804f3e1abed7fa5778e1
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-05-17 17:45:23 +0800

    add

---
+++ springboot-thymeleaf/src/main/java/com/lizp/springboot/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.lizp.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("name", "Dear");
+		return "index";
+	}
+
+}
