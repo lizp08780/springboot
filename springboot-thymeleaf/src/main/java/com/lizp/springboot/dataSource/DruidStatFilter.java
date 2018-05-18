2605d9ac9f32c7b9af781353190a66fee9f27d5e
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-05-18 12:47:01 +0800

    配置

---
+++ springboot-thymeleaf/src/main/java/com/lizp/springboot/dataSource/DruidStatFilter.java
@@ -0,0 +1,20 @@
+package com.lizp.springboot.dataSource;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+/**
+ * 
+ * @Description: druid过滤器.
+ * @author 李智鹏
+ * @date 2018年5月18日 下午12:40:56
+ *
+ */
+@WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*", initParams = {
+		@WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")// 忽略资源
+})
+public class DruidStatFilter extends WebStatFilter {
+
+}
