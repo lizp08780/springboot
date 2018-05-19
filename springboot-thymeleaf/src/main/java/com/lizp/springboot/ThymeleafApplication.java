3103afb6e5ac7519b04b04d9c951487c881af5a3
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-05-19 17:01:58 +0800

    测试

--- springboot-thymeleaf/src/main/java/com/lizp/springboot/ThymeleafApplication.java
+++ springboot-thymeleaf/src/main/java/com/lizp/springboot/ThymeleafApplication.java
@@ -1,13 +1,38 @@
 package com.lizp.springboot;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
 
 @SpringBootApplication
+// @EnableTransactionManagement
 @MapperScan("com.lizp.springboot.persist") // mapper 接口类扫描包配置
 public class ThymeleafApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafApplication.class, args);
+	}
+
+	@Bean
+	public Object testBean(PlatformTransactionManager platformTransactionManager) {
+		// org.springframework.jdbc.datasource.DataSourceTransactionManager
+		System.out.println(">>>>>>>>>>" + platformTransactionManager.getClass().getName());
+		return new Object();
+	}
+
+	@Bean
+	public Object testBean2(SqlSessionTemplate sqlSessionTemplate) {
+		System.out.println(">>>>>>>>>>" + sqlSessionTemplate.getConfiguration().getClass().getName());
+		return new Object();
+	}
+
+	@Bean
+	public Object testBean3(TransactionInterceptor transactionInterceptor) {
+		// org.springframework.transaction.annotation.AnnotationTransactionAttributeSource
+		System.out.println(">>>>>>>>>>" + transactionInterceptor.getTransactionAttributeSource().getClass().getName());
+		return new Object();
 	}
 }
--- springboot-thymeleaf/src/main/java/com/lizp/springboot/controller/IndexController.java
+++ springboot-thymeleaf/src/main/java/com/lizp/springboot/controller/IndexController.java
@@ -2,10 +2,12 @@ package com.lizp.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.lizp.springboot.annotation.Log;
 import com.lizp.springboot.persist.UserMapper;
 
 @Controller
@@ -15,12 +17,16 @@ public class IndexController {
 	private UserMapper userMapper;
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	@Transactional
 	public String index(Model model) {
-		System.err.println(userMapper.selectAll());
+		userMapper.deleteByPrimaryKey(26);
+		int x = 1 / 0;
+		System.err.println(x);
 		return "index";
 	}
 
 	@RequestMapping(value = "/index2", method = RequestMethod.GET)
+	@Log(desc = "测试记录日志")
 	public String index2(Model model) {
 		model.addAttribute("name", "Dear");
 		return "index2";
